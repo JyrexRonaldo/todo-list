768f48e6c89437dd400042d5fc6bde3b739dab98
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2023-10-24 18:24:22 +0000

    Implement task list update functionality

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import './style.css';
 
 const projects = function(){
     const _projects = {
-        bible: "yeah",
+        bible: [
+            todoItem("book", "barbel", "Last yeah", true), 
+            todoItem("book", "barbel", "tomorrow", true), 
+            todoItem("book", "barbel", "next week", true), 
+            todoItem("book", "barbel", "last month", true), 
+            todoItem("book", "barbel", "next century", true),
+        ],
         cash:  "yeah",
         money: "yeah",
         ghost: "yeah"
@@ -33,7 +39,11 @@ const projects = function(){
         return _projects[selectedProject][index]
     }
 
-    return {addProject, addTodoItem, getProjects, deleteProject, deleteTodoItem, getTodoItem}
+    const getProjectTasks = (selectedProject) => {
+        return _projects[selectedProject]
+    }
+
+    return { getProjectTasks, addProject, addTodoItem, getProjects, deleteProject, deleteTodoItem, getTodoItem }
     
 }
 
@@ -55,9 +65,16 @@ const todoItem = (title, description, dueDate, priority) => {
     return {getTodoItem, editTodoItem};
 }
 
+
+const makePayments = todoItem("make payments", "pay a lot of cash", "tomorrow", "yes very important" )
+
+makePayments.editTodoItem(null, null, "yesterday", null)
+
+console.log(makePayments.getTodoItem())
+
 const todoController = (function(){
     const _projects = projects()
-    const _selectedProject = null;
+    let _selectedProject = null;
 
     function getProjects() {
         return _projects.getProjects();
@@ -69,6 +86,10 @@ const todoController = (function(){
 
     function setSelectedProject(value) {
         _selectedProject = value;
+    }
+
+    function getSelectedProjectTasks() {
+        return _projects.getProjectTasks(_selectedProject);
     }
 
     function createProject(projectName) {
@@ -97,7 +118,7 @@ const todoController = (function(){
         _projects.getTodoItem(_selectedProject, index);
     }
 
-    return { getProjects, getSelectedProject, setSelectedProject, createProject, createTodo, deleteProject, deleteTodoItem, editTodoItem, getTodoItem}
+    return { getSelectedProjectTasks, getProjects, getSelectedProject, setSelectedProject, createProject, createTodo, deleteProject, deleteTodoItem, editTodoItem, getTodoItem}
 
 })()
 
@@ -106,16 +127,19 @@ const screenController = (function() {
     const taskDiv = document.querySelector(".task-list");
     const projectDialog = projectDiv.querySelector("dialog");
     const taskDialog = taskDiv.querySelector("dialog");
-    const projectList = projectDiv.querySelector("ul:nth-child(2)")
-    const projectListLastItem = document.querySelector(".add .add-project").parentNode.parentNode
+    const projectList = projectDiv.querySelector("ul:nth-child(2)");
+    const projectListLastItem = document.querySelector(".add .add-project").parentNode.parentNode;
+    const taskList = taskDiv.querySelector("ul");
+    const taskListLastItem = document.querySelector(".add .add-task").parentNode.parentNode;
 
-    console.log(projectListLastItem)
+
+    console.log(taskListLastItem)
 
     const updateProjectList = () => {
         const projects = todoController.getProjects();
         console.log(projects);
         for (const project in projects) {
-            createProject(project);
+            // createProject(project);
             projectList.insertBefore(createProject(project), projectListLastItem);
         }
     }
@@ -130,7 +154,38 @@ const screenController = (function() {
         iconSpan.classList.add("project");
         listItem.appendChild(projectButton)
         projectButton.append(iconSpan, projectName, deleteSpan)
-        return listItem
+        return listItem;
+    }
+
+    todoController.setSelectedProject("bible")
+
+    const updateTaskList = () => {
+        const project = todoController.getSelectedProjectTasks();
+        
+        
+        console.log(project);
+
+        project.forEach(todo => {
+            console.log(todo)
+            taskList.insertBefore(createTask(todo), taskListLastItem);
+        });
+    }
+
+    function createTask(todo) {
+        const todoItem = todo.getTodoItem();
+        const listItem = document.createElement("li");
+        const checkBox = document.createElement("input");
+        checkBox.setAttribute("type", "checkbox");
+        const taskTitle = document.createElement("p");
+        taskTitle.textContent = `${todoItem.title}`;
+        const detailsButton = document.createElement("button");
+        detailsButton.textContent = "Details";
+        const dueDatePara = document.createElement("p");
+        dueDatePara.textContent = `${todoItem.dueDate}`;
+        const editSpan = document.createElement("span");
+        const deleteSpan = document.createElement("span");
+        listItem.append(checkBox, taskTitle, detailsButton, dueDatePara, editSpan, deleteSpan)
+        return listItem;
     }
 
     projectDiv.addEventListener("click", (e)=> {
@@ -149,7 +204,8 @@ const screenController = (function() {
     
     console.log(taskDiv)
     
-    updateProjectList()
+    updateProjectList();
+    updateTaskList();
 
 })()
 
